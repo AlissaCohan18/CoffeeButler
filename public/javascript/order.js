8b141e19b4152e9c5e335187b56ade0cfc78649a
Ashley Montine Sproul <montinesproul@gmail.com>
2023-02-13 22:22:53 -0500

    added IDs to elements in dashboard.handlebars started event listeners

--- public/javascript/order.js
+++ public/javascript/order.js
@@ -22,3 +22,18 @@ const deleteOrder = (id) =>
       'Content-Type': 'application/json',
     },
   });
+
+//   let newOrder = "";
+// function placeOrder(event) {
+//     //once order is placed it is saved to db
+//     //post 
+// }
+// function deleteOrder() {
+//   //delete request 
+// }
+
+//   document.getElementById('reorder').addEventListener('click', placeOrder, true);
+
+//   document.getElementById('delete').addEventListener('click',deleteOrder, true);
+
+//   document.getElementById('order').addEventListener('click', placeOrder, true);
